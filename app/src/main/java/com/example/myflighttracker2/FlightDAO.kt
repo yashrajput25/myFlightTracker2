bb4cbc784690355e41736dd3866b28106c36c5b2
Yash Rajput <yashrajputyr10@gmail.com>
2025-04-08 03:17:02 +0530

    Update FlightDAO.kt
    interface to interact with database, consists of queries

--- app/src/main/java/com/example/myflighttracker2/FlightDAO.kt
+++ app/src/main/java/com/example/myflighttracker2/FlightDAO.kt
@@ -1,13 +1,23 @@
 package com.example.myflighttracker2
 
-import FlightRecord
-
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+@Dao
 interface FlightDAO{
-
-    suspend fun insertFlight()
-
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertFlight(flightRecord: FlightRecord)
+    @Query("SELECT * FROM flights")
     suspend fun getAll(): List<FlightRecord>
 
+
+    @Query("""
+        SELECT flightNumber, 
+               AVG(scheduledTimeMinutes + delayMinutes) AS avgDuration
+        FROM flights 
+        GROUP BY flightNumber
+    """)
     suspend fun getAverageTimePerFlight(): List<AvgTime>
 
 }
